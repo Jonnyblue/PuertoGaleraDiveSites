f40fe61ee85de1a3b70a191e8b02a0e4f49f2af1
Architeuthis_Rex <jonathanvenn@gmail.com>
2015-07-02 15:20:29 +0800

    ImageView Working, starting to look better

--- app/src/main/java/com/blueribbondivers/puertogaleradivesites/DiveSiteListFragment.java
+++ app/src/main/java/com/blueribbondivers/puertogaleradivesites/DiveSiteListFragment.java
@@ -32,72 +32,12 @@ public class DiveSiteListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.dive_site_title);
 
-        //mDivesites = new ArrayList<Divesite>();
-        //mDivesites = parse();
         mDivesites = DiveSites.get(getActivity()).getDivesites();
         DiveSiteAdaptor adapter = new DiveSiteAdaptor(mDivesites);
         setListAdapter(adapter);
 
     }
-/*
-    private ArrayList parse(){
-        Log.d(TAG, "ArrayList being parsed");
-        ArrayList<Divesite> diveSiteArray = new ArrayList<Divesite>();
-        try {
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(true);
-            XmlPullParser xpp = factory.newPullParser();
-            InputStream raw = DiveSitesActivity.mContext.getResources().openRawResource(R.raw.divesites);
-            xpp.setInput(raw,"utf-8");
-            Divesite divesite = new Divesite();
 
-            while (xpp.next() != XmlPullParser.END_DOCUMENT) {
-                String latitude = "latitude";
-                String longitude = "longitude";
-                String maxDepth = "maxDepth";
-                String name = "name";
-                String photo = "photo";
-                String siteDescription = "siteDescription";
-                String travelTime = "travelTime";
-                String youtube = "youtube";
-                String text;
-
-                if (xpp.getName().equalsIgnoreCase(latitude)) {text = xpp.nextText();
-                    divesite = new Divesite();
-                    divesite.setLatitude(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(longitude)) {text = xpp.nextText();
-                    divesite.setLongitude(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(maxDepth)) {text = xpp.nextText();
-                    divesite.setMaxDepth(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(name)) {text = xpp.nextText();
-                    divesite.setName(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(photo)) {text = xpp.nextText();
-                    divesite.setPhoto(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(siteDescription)) {text = xpp.nextText();
-                    divesite.setSiteDescription(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(travelTime)) {text = xpp.nextText();
-                    divesite.setTravelTime(text);
-                }
-                if (xpp.getName().equalsIgnoreCase(youtube)) {text = xpp.nextText();
-                    divesite.setYoutube(text);
-                    diveSiteArray.add(divesite);
-                }
-
-                xpp.next();
-            }
-        }catch(Exception e) {
-            Log.e(APP_TAG, e.getMessage());
-        }
-        return  diveSiteArray;
-
-    }
-*/
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Divesite c = ((DiveSiteAdaptor)getListAdapter()).getItem(position);
--- app/src/main/java/com/blueribbondivers/puertogaleradivesites/DivesiteFragment.java
+++ app/src/main/java/com/blueribbondivers/puertogaleradivesites/DivesiteFragment.java
@@ -1,16 +1,22 @@
 package com.blueribbondivers.puertogaleradivesites;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -26,7 +32,12 @@ public class DivesiteFragment extends Fragment {
     private TextView mDepthField;
     private TextView mSiteDescription;
     private TextView mDistance;
+    private ProportionalImageView mImageView;
     public static final String EXTRA_SITE_ID = "com.blueribbondivers.extraSiteID";
+    private Context mContext;
+
+
+
 
 
 
@@ -37,11 +48,14 @@ public class DivesiteFragment extends Fragment {
         UUID crimeId = (UUID)getActivity().getIntent()
                 .getSerializableExtra(EXTRA_SITE_ID);
         mDivesite = DiveSites.get(getActivity()).getDiveSite(crimeId);
+        mContext = getActivity().getApplicationContext();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_divesite,parent,false);
+
+        Resources resources = mContext.getResources();
 
         mTitleField = (TextView)v.findViewById(R.id.divesite_display_name);
         mTitleField.setText(mDivesite.getName());
@@ -51,7 +65,11 @@ public class DivesiteFragment extends Fragment {
         mSiteDescription.setText(mDivesite.getSiteDescription());
         mDistance = (TextView)v.findViewById(R.id.divesite_display_distance);
         mDistance.setText(mDivesite.getTravelTime());
-
+        mImageView = (ProportionalImageView)v.findViewById(R.id.divesite_display_imageView);
+        String imageName = mDivesite.getPhoto();
+        final int resourceID = resources.getIdentifier(imageName,"drawable",mContext.getPackageName());
+        Log.d("BLLAAH","Here is the resourceID " + resourceID );
+        mImageView.setImageResource(resourceID);
 
         return v;
 
